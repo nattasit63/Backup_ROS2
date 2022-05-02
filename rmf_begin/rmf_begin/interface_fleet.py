810a897cb112390d78fa9bff2bc7249ffe155712
nattasit63 <79138906+nattasit63@users.noreply.github.com>
2022-05-03 02:39:01 +0700

    Add files via upload

--- rmf_begin/rmf_begin/interface_fleet.py
+++ rmf_begin/rmf_begin/interface_fleet.py
@@ -1,10 +1,10 @@
-from logging import root
-from select import select
 import pygame as pg
 import yaml
 from tkinter import *
 from tkinter import filedialog
-
+import networkx as nx
+from networkx import DiGraph
+import matplotlib.pyplot as plt
 
 class Begin():
     def __init__(self):
@@ -12,6 +12,8 @@ class Begin():
         self.height = 720
         self.screen = pg.display.set_mode((self.width,self.height))
         pg.display.set_caption('gui')
+    def astar(self,start_node,end_node):
+        return nx.astar_path(G,str(start_node),str(end_node), heuristic = None ,weight='weight')
 
 class Select_file():
     def __init__(self):
@@ -35,16 +37,18 @@ if __name__ == '__main__':
     pg.font.init()
     begin = Begin()
     draw = Draw()
-    state = 0
+    G = DiGraph()
     num_pressadd = 0
     screen_list=[]
     pos_to_draw = [0,0,0,0]
     select_file = Select_file()
+    state=0
     print("Path to File : ",select_file.root.filename)
     select_file.root.destroy()
     with open(select_file.root.filename,'r') as f:
             yml_dict = yaml.safe_load(f)
     image_file = yml_dict.get('drawing')
+
     bg = pg.image.load(image_file)
     bg = pg.transform.scale(bg, (begin.width, begin.height))
     rect = bg.get_rect()
@@ -73,14 +77,34 @@ if __name__ == '__main__':
                     screen_list.append(current_screen)
                     draw.circle(mouse[0],mouse[1],begin.width//100)
                     num_pressadd+=1
-                    # draw.line(pos_to_draw[0],pos_to_draw[1],120,120)
                     
                 elif event.type == pg.MOUSEBUTTONDOWN and event.button==3:
                     if num_pressadd>=0:
                         num_pressadd = num_pressadd-1
                         begin.screen.blit(current_screen,(0,0))
-
             pg.display.update()
+            state=2
 
+        elif state==2:
+            node_list_yaml = yml_dict.get('node_list')
+            edge_list_yaml = yml_dict.get('edge_list')
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    exit()
+            for n,p in node_list_yaml.items():
+                G.add_node(n,pos=p)
+            for s,e in edge_list_yaml.items():
+                G.add_edge(str(e[0]),str(e[1]),weight = e[2])
+     
+            pos = nx.get_node_attributes(G,'pos')
+            labels = nx.get_edge_attributes(G,'weight')
+            nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+            nx.draw(G,pos,with_labels = True,arrows = True)
+            
+            print(begin.astar(0,3))
 
-   
+            plt.show()
+            
+        
+        
