5aa261f31a89743b68f3eb93813437f39984ddd9
nattasit63 <79138906+nattasit63@users.noreply.github.com>
2022-05-22 03:14:11 +0700

    Prepare to VRPY

--- rmf_begin/rmf_begin/interface_fleet.py
+++ rmf_begin/rmf_begin/interface_fleet.py
@@ -1,3 +1,4 @@
+from array import array
 import pygame as pg
 import yaml
 from tkinter import *
@@ -6,6 +7,9 @@ import networkx as nx
 from networkx import DiGraph
 import matplotlib.pyplot as plt
 import numpy as np
+from networkx import DiGraph, from_numpy_matrix, relabel_nodes, set_node_attributes
+from numpy import array
+
 
 class Begin():
     def __init__(self):
@@ -26,6 +30,10 @@ class Select_file():
         self.node_list_yaml = yml_dict.get('node_list')
         self.edge_list_yaml = yml_dict.get('edge_list')
         self.assign_node = yml_dict.get('assign_list')
+        self.demand_node_yaml = yml_dict['demand_node']
+        self.timeread=0
+        self.A_matrix = []
+
     def write_file(self,data,filename):
         print('Writing to file . . .')
         fname = filename
@@ -38,9 +46,11 @@ class Select_file():
         fname = filename
         with open(fname,"r") as f:
             loaded = yaml.safe_load(f)
+        self.A_matrix = loaded.get('Adjacency Matrix')
         loaded = np.array(loaded)
-        print(loaded)
+        # print(loaded)
         print('Read Done !')
+        
         return loaded
 
 
@@ -55,24 +65,32 @@ class Draw():
         pg.draw.line(begin.screen, (255, 0, 0), (x1, y1), (x2, y2))
         self.flag_draw =1
         pg.display.flip()
+    
+    def show_nx_graph(self):
+        pos = nx.get_node_attributes(G,'pos')
+        labels = nx.get_edge_attributes(G,'weight')
+        nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+        nx.draw(G,pos,with_labels = True,arrows = True)
+        plt.show()
+        
 
 class NX():
     def __init__(self):
         self.pickup_node =[]
         self.depot_node =[]
         self.dropoff_node =[]
-
         self.nodelist=[]
         self.astar_path = 0
         self.astar_path_cost=[]
         self.adjMatrix=[]
         self.isPathAavailable=0
+        self.demand_list=dict
+
     def astar(self,start_node,end_node):
         self.astar_path = nx.astar_path(G,str(start_node),str(end_node), heuristic = None ,weight='weight')
         return self.astar_path
 
     def adjacency_matrix(self,node_ls):
-        n=0
         sum=0
         size = len(node_ls)
         self.adjMatrix = np.zeros((size,size))
@@ -85,23 +103,18 @@ class NX():
                 #2 = No Path  
                 if i==j :
                     flag = 1
-                
                 try:
                     path = self.astar(i,j)             
                 except :
                     flag = 2
-                
                 if(flag == 0):
                     path = self.astar(i,j)
-                    
                     path_size = len(path)
-
                     for k in range(path_size-1):
                         for s,e in select_file.edge_list_yaml.items():
                             if(int(path[k])==e[0] and int(path[k+1])==e[1]):
                                 sum += e[2]
                                 break
-
                     for s,e in select_file.edge_list_yaml.items():
                         if int(i)==int(e[0]):
                             if int(j)==int(e[1]):
@@ -109,16 +122,15 @@ class NX():
                                     sum = min(e[2],sum)
                                 else:
                                     sum = e[2]
-                self.adjMatrix[i][j]= sum
-                    
-
+                self.adjMatrix[i][j]= sum             
         # print(path)
         # print(sum)                  
-       
         # print(self.adjMatrix)
         return self.adjMatrix
                 
                
+
+
 
 
 if __name__ == '__main__':
@@ -187,25 +199,18 @@ if __name__ == '__main__':
                 if c == "drop off":
                     nw.dropoff_node+=v
 
-           
-        ##Test A* path
-            # test_astar = nw.astar(0,3)
-            # print(test_astar)
+            #DEMAND node : amount
+            nw.demand_list = select_file.demand_node_yaml
 
+            #Sum a* path cost 
             adj_matrix = nw.adjacency_matrix(nw.nodelist) 
 
-        ## write file
+            # Write file
             # select_file.write_file({"Adjacency Matrix": np.matrix.tolist(adj_matrix)},'QQQ.yaml')  
         
-        ## read file
-            #select_file.read_matrix('QQQ.yaml')
+            # Read file
+            select_file.read_matrix('QQQ.yaml')
 
-            pos = nx.get_node_attributes(G,'pos')
-            labels = nx.get_edge_attributes(G,'weight')
-            nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-            nx.draw(G,pos,with_labels = True,arrows = True)
-
-            plt.show()
             state =3
 
 
@@ -215,6 +220,22 @@ if __name__ == '__main__':
                     pg.quit()
                     exit()
             
+           
+
+           
+            if select_file.timeread==0:
+                A = select_file.A_matrix
+                A = np.array(A)
+                A = array(A,dtype=[("cost", int)])
+                B = from_numpy_matrix(A, create_using=nx.DiGraph())
+                set_node_attributes(B, values=nw.demand_list, name="demand")
+                B = relabel_nodes(B, {0: "Source", 10: "Sink"})
+
+                select_file.timeread = 1
+
+            draw.show_nx_graph()
+   
+
             
         
       
