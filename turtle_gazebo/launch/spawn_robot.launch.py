27384b4f750486f49670395be53885c25d849e21
nattasit63 <79138906+nattasit63@users.noreply.github.com>
2022-05-30 00:48:16 +0700

    Add files via upload

--- turtle_gazebo/launch/spawn_robot.launch.py
+++ turtle_gazebo/launch/spawn_robot.launch.py
@@ -7,16 +7,22 @@ from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
 import xacro
-def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
-    world_file_name = 'nothing.world'
-    pkg_dir = get_package_share_directory('turtle_gazebo')
-    os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'urdf')
 
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    pkg_dir = get_package_share_directory('turtle_gazebo')
+    world_file_name = 'nothing.world'
     world = os.path.join(pkg_dir, 'world', world_file_name)
-    launch_file_dir = os.path.join(pkg_dir, 'launch')
+
+    os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'urdf')
     robot_description_path =  os.path.join(pkg_dir,"urdf","model.sdf",)
     robot_description = {"robot_description": xacro.process_file(robot_description_path).toxml()}
+    
+    launch_file_dir = os.path.join(pkg_dir, 'launch')
+
+    
  
     gazebo = ExecuteProcess(cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],output='screen')
     spawn_entity = Node(package='turtle_gazebo', executable='spawn_turtle',arguments=['robot1', 'robot1', '1', '0', '0.0'],output='screen')
@@ -30,6 +36,7 @@ def generate_launch_description():
         spawn_entity2,
         #follow_first_robot,
         #turtlesim_node,
-    
-
     ])
+
+
+
